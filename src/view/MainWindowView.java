59ff6142d56f863c6139bcc45cd824eb16159eca
Yuval <yuvalmich2@gmail.com>
2021-01-04 12:13:49 +0200

    add main window view layer

---
+++ src/view/MainWindowView.java
@@ -0,0 +1,17 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+
+public class MainWindowView {
+
+	@FXML
+	Button connectServerButton;
+	
+	@FXML
+	public void onConnectServerButtonClicked() {
+		
+	}
+	
+}
